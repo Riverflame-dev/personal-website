daefac7543051ef935189b3606fc70a0f1dacc94
Riverflame-dev <the.yilei.li@gmail.com>
2022-09-01 18:24:54 -0400

    updated project section and project content

--- src/pages/home/projects/index.js
+++ src/pages/home/projects/index.js
@@ -10,37 +10,70 @@ export default function Projects() {
         <h1>Projects</h1>
         <p>Here i will be displaying some projects that I was involved in.</p>
       </div>
-      <div className="featured-project">
-        <div className="featured-content">
-          <p class="project-overline">Featured Project</p>
-          <h3 class="project-title">
-            <a href="https://halcyon-theme.netlify.com/" rel="noopener noreferrer" target="_blank">
-              Project 1
-            </a>
-          </h3>
-          <div class="project-description">
-            <p>A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on...</p>
+      <ul className="featured-list">
+        <li className="featured-project">
+          <div className="featured-content">
+            <p class="project-overline">Featured Project</p>
+            <h3 class="project-title">
+              <a href="https://halcyon-theme.netlify.com/" rel="noopener noreferrer" target="_blank">
+                Project 1
+              </a>
+            </h3>
+            <div class="project-description">
+              <p>Design and implement changes to the Product Detail Page, in an attempt to provide efficient product information and improve  customer experience.</p>
+            </div>
+            <ul class="project-tech-list">
+              <li>jQuary</li>
+              <li>Backbone.js</li>
+              <li>SCSS</li>
+              <li>HTML5</li>
+              <li>Shopify</li>
+            </ul>
+            <div class="project-links">
+              <a href="https://github.com/bchiang7/halcyon-site" aria-label="File Link" rel="noopener noreferrer" target="_blank">
+                <FaRegFile className="link-icon"/>
+              </a>
+              <a href="https://verabradley.com/products/xl-campus-backpack-2845413063?variant=40445993156652" aria-label="External Link" class="external" rel="noopener noreferrer" target="_blank">
+                <FaExternalLinkAlt className="link-icon"/>
+              </a>
+            </div>
           </div>
-          <ul class="project-tech-list">
-            <li>VS Code</li>
-            <li>Sublime Text</li>
-            <li>Atom</li>
-            <li>iTerm2</li>
-            <li>Hyper</li>
-          </ul>
-          <div class="project-links">
-            <a href="https://github.com/bchiang7/halcyon-site" aria-label="File Link" rel="noopener noreferrer" target="_blank">
-              <FaRegFile className="link-icon"/>
-            </a>
-            <a href="https://halcyon-theme.netlify.com/" aria-label="External Link" class="external" rel="noopener noreferrer" target="_blank">
-              <FaExternalLinkAlt className="link-icon"/>
-            </a>
+          <div className="featured-img">
+            <img aria-hidden="true" src={'/assets/VBproject1.png'} alt=""/>
           </div>
-        </div>
-        <div className="featured-img">
-        <img aria-hidden="true" src={'/assets/VBproject1.png'} alt=""/>
-        </div>
-      </div>
+        </li>
+        <li className="featured-project">
+          <div className="featured-content">
+            <p class="project-overline">Featured Project</p>
+            <h3 class="project-title">
+              <a href="https://halcyon-theme.netlify.com/" rel="noopener noreferrer" target="_blank">
+                VB Live
+              </a>
+            </h3>
+            <div class="project-description">
+              <p>A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on...</p>
+            </div>
+            <ul class="project-tech-list">
+              <li>jQuary</li>
+              <li>Backbone.js</li>
+              <li>SCSS</li>
+              <li>HTML5</li>
+              <li>Shopify</li>
+            </ul>
+            <div class="project-links">
+              <a href="https://github.com/bchiang7/halcyon-site" aria-label="File Link" rel="noopener noreferrer" target="_blank">
+                <FaRegFile className="link-icon"/>
+              </a>
+              <a href="https://verabradley.com/products/xl-campus-backpack-2845413063?variant=40445993156652" aria-label="External Link" class="external" rel="noopener noreferrer" target="_blank">
+                <FaExternalLinkAlt className="link-icon"/>
+              </a>
+            </div>
+          </div>
+          <div className="featured-img">
+            <img aria-hidden="true" src={'/assets/VBproject2.png'} alt=""/>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
