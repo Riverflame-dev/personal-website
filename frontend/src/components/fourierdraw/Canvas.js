5e06706254f6f90485a60b6e0a1aca140f41d20b
Riverflame-dev <the.yilei.li@gmail.com>
2022-09-23 22:28:10 -0400

    updated Canvas

--- frontend/src/components/fourierdraw/Canvas.js
+++ frontend/src/components/fourierdraw/Canvas.js
@@ -1,14 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Canvas = ({ id, draw, height, width }) => {
+const Canvas = ({ id, draw, height, width, lineWidth, enlarge, cx, cy }) => {
   const canvas = React.useRef();
 
   React.useEffect(() => {
     const ctx = canvas.current.getContext("2d");
     canvas.width = width;
     canvas.height = height;
-    draw(ctx, height, width);
+    draw(ctx, height, width, lineWidth, enlarge, cx, cy);
   });
 
   return <canvas id={id} ref={canvas} />;
@@ -18,7 +18,8 @@ Canvas.propTypes = {
   id: PropTypes.func.isRequired,
   draw: PropTypes.func.isRequired,
   height: PropTypes.number.isRequired,
-  width: PropTypes.number.isRequired
+  width: PropTypes.number.isRequired,
+  lineWidth: PropTypes.number.isRequired
 };
 
 export default Canvas;
